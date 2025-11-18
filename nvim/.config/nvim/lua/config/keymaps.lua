410d96264e824da4f97ba4236f3b3b726e076667
amstrdm <a.caserta@proton.me>
2025-11-18 13:07:15 +0100

    add git worktree plugin

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,1 +1,14 @@
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit Terminal Mode" })
+
+vim.keymap.set(
+  "n",
+  "<leader>fW",
+  "<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<cr>",
+  { desc = "Find Worktrees" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>fw",
+  "<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<cr>",
+  { desc = "New Worktree" }
+)
