3c15893ea1d723b728355ed6d086b2cd6a460ca6
amstrdm <a.caserta@proton.me>
2025-11-21 18:36:03 +0100

    added keymap for toggling python type diagnostics

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -12,3 +12,27 @@ vim.keymap.set(
   "<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<cr>",
   { desc = "New Worktree" }
 )
+
+vim.keymap.set("n", "<leader>ut", function()
+  -- 1. Find the BasedPyright client
+  local clients = vim.lsp.get_clients({ name = "basedpyright" })
+  if #clients == 0 then
+    vim.notify("BasedPyright is not active.", vim.log.levels.WARN)
+    return
+  end
+
+  -- 2. Get the specific namespace ID for BasedPyright
+  local client = clients[1]
+  local ns = vim.lsp.diagnostic.get_namespace(client.id)
+
+  -- 3. Check if currently enabled and toggle
+  _G.basedpyright_off = not _G.basedpyright_off
+
+  if _G.basedpyright_off then
+    vim.diagnostic.enable(false, { ns_id = ns })
+    vim.notify("BasedPyright (Types) Hidden", vim.log.levels.INFO)
+  else
+    vim.diagnostic.enable(true, { ns_id = ns })
+    vim.notify("BasedPyright (Types) Visible", vim.log.levels.INFO)
+  end
+end, { desc = "Toggle Type Diagnostics" })
