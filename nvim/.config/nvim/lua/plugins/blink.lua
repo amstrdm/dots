7f5f159c141b93ded82717829b46ef84f5f973c9
amstrdm <a.caserta@proton.me>
2025-12-07 14:16:34 +0100

    add codesnap

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -2,24 +2,7 @@ return {
   {
     "saghen/blink.cmp",
     opts = {
-      -- turn off the preset so our keys take effect
-      keymap = {
-        preset = "none",
-        -- accept with Tab; if in snippet, jump; otherwise fall back
-        ["<Tab>"] = { "accept", "snippet_forward", "fallback" },
-        ["<S-Tab>"] = { "snippet_backward", "fallback" },
-
-        -- navigate suggestions
-        ["<C-j>"] = { "select_next" },
-        ["<C-k>"] = { "select_prev" },
-
-        -- scroll docs
-        ["<C-h>"] = { "scroll_documentation_up" },
-        ["<C-l>"] = { "scroll_documentation_down" },
-
-        -- make Enter a normal newline
-        ["<CR>"] = { "fallback" },
-      },
+      fuzzy = { implementation = "lua" },
     },
   },
 }
---
+++ nvim/.config/nvim/lua/plugins/codesnap.lua
@@ -0,0 +1,15 @@
+return {
+  "mistricky/codesnap.nvim",
+  opts = {
+    snapshot_config = {
+      watermark = {
+        content = "",
+
+        font_family = "Pacifico",
+        color = "#ffffff",
+      },
+    },
+
+    show_line_number = true,
+  },
+}
